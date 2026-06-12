7c0cff61e403b52c311df303398baa50578ba2b8
Mark Mote <137040517+markmote1@users.noreply.github.com>
2026-06-12 21:22:54 +0300

    Playing with javascript

--- todo.js
+++ todo.js
@@ -4,11 +4,37 @@ let todos = document.querySelector("#todos")
 let dateHolder = document.querySelector("#dateHolder")
 
 console.dir(inputTodo)
+let arr = []
 
-addTodoBtn.addEventListener('click', ()=>{
-let content = JSON.stringify(inputTodo.value)
-localStorage.setItem('Todo', content)
-console.log(content)
+addTodoBtn.addEventListener('click',()=>{
+    addTodo(inputTodo.value)
+    inputTodo.value = ''
 })
 
 
+function addTodo(text){
+   if(text.trim() === '') return
+   const todo = {
+    id: Date.now(),
+    text,
+    completed: false
+   }
+   arr.push(todo)
+   console.log(arr)
+   saveTodos()
+   getTodos()
+
+}
+
+
+function saveTodos(){
+    localStorage.setItem('todo',JSON.stringify(arr))
+}
+
+
+function getTodos(){
+    arr.forEach((a)=>{
+        console.log(a)
+    })
+
+}
