2387913e60db4c6102ba899c317d189835bfbed1
Mark Mote <137040517+markmote1@users.noreply.github.com>
2026-06-13 13:04:13 +0300

    Still playing with javascript

--- todo.js
+++ todo.js
@@ -4,37 +4,3 @@ let todos = document.querySelector("#todos")
 let dateHolder = document.querySelector("#dateHolder")
 
 console.dir(inputTodo)
-let arr = []
-
-addTodoBtn.addEventListener('click',()=>{
-    addTodo(inputTodo.value)
-    inputTodo.value = ''
-})
-
-
-function addTodo(text){
-   if(text.trim() === '') return
-   const todo = {
-    id: Date.now(),
-    text,
-    completed: false
-   }
-   arr.push(todo)
-   console.log(arr)
-   saveTodos()
-   getTodos()
-
-}
-
-
-function saveTodos(){
-    localStorage.setItem('todo',JSON.stringify(arr))
-}
-
-
-function getTodos(){
-    arr.forEach((a)=>{
-        console.log(a)
-    })
-
-}
